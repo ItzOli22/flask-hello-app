4d1d87e406706ace71c6094e83b31c6926481d19
hello <lelian123456@gmail.com>
2025-05-05 22:10:47 +0800

    Initial commit

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    return f"Thank you for submitting, {name}! We will contact you at {email}."
 
 if __name__ == '__main__':
     app.run(debug=True)
